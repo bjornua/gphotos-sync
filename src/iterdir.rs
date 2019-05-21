5d726b42349d252c827d8fb2edcfe6e92b99624e
Bjørn Arnholtz <bjorn.arnholtz@gmail.com>
2019-05-21 15:29:04 +0200

    Stuff

---
+++ src/iterdir.rs
@@ -0,0 +1,142 @@
+use std::ffi::{OsStr, OsString};
+use std::fs::{read_dir, DirEntry, ReadDir};
+
+#[derive(Debug)]
+pub enum IterDirError {
+    ReadDirError {
+        directory_path: OsString,
+        cause: std::io::Error,
+    },
+    ReadDirIterateError {
+        directory_path: OsString,
+        cause: std::io::Error,
+    },
+    GetFileTypeError {
+        dir_entry: DirEntry,
+        cause: std::io::Error,
+    },
+    NotRegularFile {
+        dir_entry: DirEntry,
+    },
+}
+
+pub struct RecursiveIterDir {
+    directories: Vec<OsString>,
+    cur: Option<(OsString, ReadDir)>,
+}
+impl RecursiveIterDir {
+    pub fn new(root_directory: OsString) -> Self {
+        Self {
+            directories: vec![root_directory],
+            cur: None,
+        }
+    }
+}
+impl Iterator for RecursiveIterDir {
+    type Item = Result<DirEntry, IterDirError>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            let (dir_path, dir_iter) = match self.cur {
+                Some(ref mut x) => x,
+                None => {
+                    let dir = self.directories.pop()?;
+                    match read_dir(&dir) {
+                        Ok(dir_iter) => {
+                            self.cur = Some((dir, dir_iter));
+                            self.cur.as_mut().unwrap()
+                        }
+                        Err(e) => {
+                            return Some(Err(IterDirError::ReadDirError {
+                                cause: e,
+                                directory_path: dir,
+                            }));
+                        }
+                    }
+                }
+            };
+            let dir_entry = match dir_iter.next() {
+                Some(Ok(dir_entry)) => dir_entry,
+                Some(Err(e)) => {
+                    return Some(Err(IterDirError::ReadDirIterateError {
+                        cause: e,
+                        directory_path: dir_path.to_owned(),
+                    }));
+                }
+                None => {
+                    // Current directory finished
+                    self.cur = None;
+                    continue;
+                }
+            };
+
+            // let path = dir_entry.path().into_os_string();
+            match dir_entry.file_type() {
+                Ok(file_type) if file_type.is_dir() => {
+                    self.directories.push(dir_entry.path().into_os_string());
+                    // println!("{}", self.directories.len());
+                    continue;
+                }
+                Ok(file_type) if file_type.is_file() => return Some(Ok(dir_entry)),
+                Ok(_) => return Some(Err(IterDirError::NotRegularFile { dir_entry })),
+                Err(e) => {
+                    return Some(Err(IterDirError::GetFileTypeError {
+                        dir_entry,
+                        cause: e,
+                    }));
+                }
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Match {
+    pub dir_entry: DirEntry,
+    pub extension: OsString,
+}
+
+#[derive(Debug)]
+pub enum FindFilesError {
+    WrongExtension {
+        dir_entry: DirEntry,
+        extension: OsString,
+    },
+    MissingExtension {
+        dir_entry: DirEntry,
+    },
+    IterDirError(IterDirError),
+}
+
+pub fn findfiles<T: Into<OsString>, U: IntoIterator<Item = T>>(
+    directory: OsString,
+    extensions: U,
+) -> impl Iterator<Item = Result<Match, FindFilesError>> {
+    let files = RecursiveIterDir::new(directory);
+    let extensions: Vec<OsString> = extensions.into_iter().map(|x| x.into()).collect();
+
+    let files = files.map(move |file| {
+        let dir_entry = match file {
+            Ok(direntry) => direntry,
+            Err(e) => return Err(FindFilesError::IterDirError(e)),
+        };
+        let path = dir_entry.path();
+        let extension = match path.extension() {
+            Some(extension) => extension,
+            None => return Err(FindFilesError::MissingExtension { dir_entry }),
+        };
+
+        if !extensions.iter().any(|e| e == extension) {
+            return Err(FindFilesError::WrongExtension {
+                dir_entry,
+                extension: extension.to_owned(),
+            });
+        };
+
+        return Ok(Match {
+            dir_entry,
+            extension: extension.to_owned(),
+        });
+    });
+    files
+}
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ Goal:
 
 Features:
     Generate list of file to process
+    Detect images
     Detect sd-card is inserted
     Delete successfuly uploaded files
     Send notification (email, IM, whatever) when certain events happens
@@ -22,98 +23,37 @@ Features:
 
 */
 
+mod iterdir;
 use std::ffi::OsString;
-use std::fs;
-use std::io::Write;
 
-fn main() {
-    let directory = if let Some(arg) = std::env::args_os().skip(1).next() {
-        arg
-    } else {
-        return;
-    };
-    let (mut paths, errors) = walk_dir(OsString::from(directory));
-    paths.sort();
-    let stdout = std::io::stdout();
-    let mut stdout_handle = stdout.lock();
-    for path in paths {
-        stdout_handle.write(path.to_str().unwrap().as_bytes());
-        stdout_handle.write(b"\n");
-    }
-    // println!("{:#?}", paths);
-    // println!("{:#?}", errors);
-}
-
-#[derive(Debug)]
-enum WalkDirError {
-    ReadDirError {
-        path: OsString,
-        cause: std::io::Error,
-    },
-    ReadDirIterateError {
-        path: OsString,
-        cause: std::io::Error,
-    },
-    GetFileTypeError {
-        path: OsString,
-        cause: std::io::Error,
-    },
-    NotRegularFile {
-        path: OsString,
-    },
-}
-
-fn walk_dir(root_directory: OsString) -> (Vec<OsString>, Vec<WalkDirError>) {
-    let mut errors: Vec<WalkDirError> = Vec::with_capacity(0);
-    let mut directories: Vec<OsString> = Vec::with_capacity(20);
-    let mut files: Vec<OsString> = Vec::with_capacity(200);
-
-    directories.push(root_directory);
-    while let Some(directory) = directories.pop() {
-        let paths = match fs::read_dir(&directory) {
-            Ok(paths) => paths,
-            Err(e) => {
-                errors.push(WalkDirError::ReadDirError {
-                    path: directory.to_owned(),
-                    cause: e,
-                });
-                continue;
-            }
-        };
-        for path_result in paths {
-            let path = match path_result {
-                Ok(path) => path,
-                Err(e) => {
-                    errors.push(WalkDirError::ReadDirIterateError {
-                        path: directory.to_owned(),
-                        cause: e,
-                    });
-                    continue;
-                }
-            };
-            let filetype = match path.file_type() {
-                Ok(filetype) => filetype,
-                Err(e) => {
-                    errors.push(WalkDirError::GetFileTypeError {
-                        path: directory.to_owned(),
-                        cause: e,
-                    });
-                    continue;
-                }
-            };
-            if filetype.is_dir() {
-                directories.push(path.path().into_os_string());
-                continue;
-            }
-            if filetype.is_file() {
-                files.push(path.path().into_os_string());
-                continue;
-            }
-            errors.push(WalkDirError::NotRegularFile {
-                path: path.path().into_os_string(),
-            });
+fn main() -> () {
+    let args = match get_args() {
+        Ok(v) => v,
+        Err(e) => {
+            println!("{}", e);
+            return;
         }
-    }
+    };
+    let files = crate::iterdir::findfiles(args.directory, args.extensions)
+        .filter_map(Result::ok)
+        .map(|m| m.dir_entry.path());
+    println!("{:#?}", files.count())
+}
 
-    return (files, errors);
+struct Args {
+    directory: OsString,
+    extensions: Vec<OsString>,
+}
+fn get_args() -> Result<Args, &'static str> {
+    let mut args = std::env::args_os().skip(1);
+    let directory = match args.next() {
+        Some(directory) => directory,
+        None => return Err("Invalid directory"),
+    };
+    let extensions: Vec<_> = args.collect();
+
+    return Ok(Args {
+        directory,
+        extensions,
+    });
 }
