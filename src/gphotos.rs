e6fb60bd9e1cbc32a2642a707eb6caf641e3351f
Bjorn Arnholtz <bjorn.arnholtz@gmail.com>
2019-09-08 23:47:38 +0200

    Added some google oauth stuff

--- src/gphotos.rs
+++ src/gphotos.rs
@@ -3,6 +3,7 @@ use crate::open;
 use futures::{sync::oneshot::channel, Canceled, Future};
 use hyper::service::service_fn_ok;
 use hyper::{Body, Request, Response, Server};
+use percent_encoding::percent_encode;
 use std::cell::RefCell;
 use std::io;
 use std::rc::Rc;
@@ -13,9 +14,19 @@ enum OpenAuthenticationURLError {
     IOError(io::Error),
 }
 
+const SCOPE: &str = "https://www.googleapis.com/auth/photoslibrary.readonly";
+const CLIENT_ID: &str = "529339861110-tppfskj6n90kk71ic9tqetm3l7471p6q.apps.googleusercontent.com";
+
+fn get_oauth_url(port: u16) -> String {
+    let redirect_uri: String = format!("http://127.0.0.1:{}/", port);
+
+    return format!("https://accounts.google.com/o/oauth2/v2/auth?redirect_uri={}&prompt=consent&response_type=code&client_id={}&scope={}&access_type=offline", redirect_uri, CLIENT_ID, SCOPE);
+}
+
+// const
+
 fn open_authentication_url(port: u16) -> Result<(), OpenAuthenticationURLError> {
-    let status = open::that(format!("http://127.0.0.1:{}", port))
-        .map_err(OpenAuthenticationURLError::IOError)?;
+    let status = open::that(get_oauth_url(port)).map_err(OpenAuthenticationURLError::IOError)?;
     match status.success() {
         true => Ok(()),
         false => Err(OpenAuthenticationURLError::NonZeroExitCode),
